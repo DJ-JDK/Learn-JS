5002267f0000c3445c532134b320d06ce6b7d859
DJ-JDK <74508959+DJ-JDK@users.noreply.github.com>
2024-06-27 14:29:03 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 144553
+let accountEmail = "Kinsung@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity])
